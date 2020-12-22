23dac93b02602edc226cf8dd28c873f9e98f1c54
abdulgonicse <abdulgoni.me@gmail.com>
2020-12-22 16:39:54 +0600

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LayoutController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [LayoutController::class, 'index']);
+
+Route::get('/user/list',[ManagementController::class, 'UserList'])->name('user.list');
