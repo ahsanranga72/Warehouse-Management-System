67aa71372fac1a2398cf8ef545d999e200cc708e
Ahsan <ahsan.ranga@gmail.com>
2020-12-26 20:07:24 +0600

    add product verified

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,32 +4,70 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\AddProduct;
+use App\Models\Product\Product;
+use App\Models\Product\ProductType;
+use App\Models\Product\BarcodeSymbology;
+use App\Models\Product\Brand;
+use App\Models\Product\Category;
+use App\Models\Product\ProductUnit;
+use App\Models\Product\PurchaseUnit;
+use App\Models\Product\SaleUnit;
+use App\Models\Product\TaxMethod;
+use Response;
 
 class ProductController extends Controller
 {
     public function addproduct(){
-        return view('product.add-product');
+        
+        $data=ProductType::all();
+        $bar=BarcodeSymbology::all();
+        $brand=Brand::all();
+        $category=Category::all();
+        $prounit=ProductUnit::all();
+        $purunit=PurchaseUnit::all();
+        $salunit=SaleUnit::all();
+        $tax=TaxMethod::all();
+        return view('product.add-product',compact('data','bar','brand','category','prounit','purunit','salunit','tax'));
+        //return view('product.add-product',['bar'=>$bar]);
     }
 
     public function storeproduct(Request $request){
-
-        $product = New AddProduct;
-        $product->productType = $request->productType;
-        $product->productName = $request->productName;
-        $product->productCode = $request->productCode;
-        $product->barcodeSymbology = $request->barcodeSymbology;
+        //  print_r($request->all());die();
+        $product = New Product;
+        $product->product_type = $request->product_type;
+        $product->product_name = $request->product_name;
+        $product->product_code = $request->product_code;
+        $product->barcode_symbology = $request->barcode_symbology;
         $product->brand = $request->brand;
         $product->category = $request->category;
-        $product->productUnit = $request->productUnit;
-        $product->saleUnit = $request->saleUnit;
-        $product->purchaseUnit = $request->purchaseUnit;
-        $product->productCost = $request->productCost;
-        $product->productPrice = $request->productPrice;
-        $product->productTax = $request->productTax;
-        $product->taxMethod = $request->taxMethod;
-        $product->productImage = $request->productImage;
-        $product->productDetails = $request->summernote;
-        $product->save();
+        $product->product_unit = $request->product_unit;
+        $product->sale_unit = $request->sale_unit;
+        $product->purchase_unit = $request->purchase_unit;
+        $product->product_cost = $request->product_cost;
+        $product->product_price = $request->product_price;
+        $product->alert_quantity = $request->alert_quantity;
+        $product->product_tax = $request->product_tax;
+        $product->tax_method = $request->tax_method;
+        $product->product_details = $request->product_details;
+        
+        if($request->hasfile('product_image')){
+            $file = $request->file('product_image');
+            $extention = $file->getClientOriginalExtension();
+            $filename =date('mdYHis') . uniqid() .'.'.$extention;
+            $file->move('upload/product_images/',$filename);
+            $product->product_image = $filename;
+        } else {
+            //return $request;
+            $product->product_image = "";
+        }
+     
+           $save = $product->save();
+           if($save){
+            return Response::json(array('success' => 'true', 'message' => 'Product has been added succesefully.'));
+           }else{
+            return Response::json(array('success' => 'false', 'message' => 'Product has not been added succesefully.'));
+           }
+
+
     }
 }
---
+++ database/migrations/2020_12_24_070254_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->Integer('product_type')->nullable();
+            $table->String('product_name',50)->nullable();
+            $table->String('product_code',50)->nullable();
+            $table->Integer('barcode_symbology')->nullable();
+            $table->Integer('brand')->nullable();
+            $table->Integer('category')->nullable();
+            $table->Integer('product_unit')->nullable();
+            $table->Integer('sale_unit')->nullable();
+            $table->Integer('purchase_unit')->nullable();
+            $table->Float('product_cost')->nullable();
+            $table->Float('product_price')->nullable();
+            $table->Float('alert_quantity')->nullable();
+            $table->Float('product_tax')->nullable();
+            $table->Integer('tax_method')->nullable();
+            $table->String('product_image',255)->nullable();
+            $table->Text('product_details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
